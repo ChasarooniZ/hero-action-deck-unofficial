9affd94bd7e93c6e4772fc20de50e35bf6d3c020
cadowtin <chasdowtin@gmail.com>
2026-08-27 14:47:58 -0500

    solo/hero aciton work

--- scripts/ui.js
+++ scripts/ui.js
@@ -133,7 +133,6 @@ async function cardAction(cardID) {
 }
 
 function getHeroPointArt(val, max) {
-  const empty = max - val;
-  const res = "".padStart(empty, "◇").padEnd(max, "◈");
+  const res = "".padStart(val, "✪").padEnd(max, "⨀");
   return res;
 }
